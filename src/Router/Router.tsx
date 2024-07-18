e7bfe1c55e2e575e2ee20e3806599dd8b0bb8a03
MomirE3 <acamomir1@gmail.com>
2024-07-19 00:22:40 +0200

    added dashboard

--- src/Router/Router.tsx
+++ src/Router/Router.tsx
@@ -1,40 +1,38 @@
-import {
-    RouterProvider,
-    createBrowserRouter,
-} from "react-router-dom";
-import { RoutesNames } from "./Routes";
-import { LoginPage } from "../pages/LoginPage";
-import { RegisterPage } from "../pages/RegisterPage";
-import { HomePage } from "../pages/Home";
-import { AuthService } from "../Services/AuthService";
-import { JWTStorage } from "../Services/JWTStorage";
-import { PrivateRoute } from "./PrivateRoute";
-import { BlobService } from "../Services/BlobService";
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { RoutesNames } from './Routes';
+import { LoginPage } from '../pages/LoginPage';
+import { RegisterPage } from '../pages/RegisterPage';
+import { HomePage } from '../pages/Home';
+import { AuthService } from '../Services/AuthService';
+import { JWTStorage } from '../Services/JWTStorage';
+import { PrivateRoute } from './PrivateRoute';
+import { BlobService } from '../Services/BlobService';
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <PrivateRoute jwtStorage={JWTStorage}/>,
-        children: [
-          {
-            path: '',
-            element: <HomePage />,
-          },
-        ],
-    },
-    {
-        path: `/${RoutesNames.Login}`,
-        element: <LoginPage authService={AuthService} jwtStorage={JWTStorage}/>,
-    },
-    {
-        path: `/${RoutesNames.Register}`,
-        element: <RegisterPage authService={AuthService} blobService={BlobService}/>,
-    },
-
+	{
+		path: '/',
+		// element: <PrivateRoute jwtStorage={JWTStorage}/>,
+		children: [
+			{
+				path: '',
+				element: <HomePage />,
+			},
+		],
+	},
+	{
+		path: `/${RoutesNames.Login}`,
+		element: (
+			<LoginPage authService={AuthService} jwtStorage={JWTStorage} />
+		),
+	},
+	{
+		path: `/${RoutesNames.Register}`,
+		element: (
+			<RegisterPage authService={AuthService} blobService={BlobService} />
+		),
+	},
 ]);
 
 export function Router() {
-    return (
-        <RouterProvider router={router} />
-    );
+	return <RouterProvider router={router} />;
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,65 @@
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import { RoutesNames } from "../Router/Routes";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from './Home.module.css';
 
-export function HomePage(){
-    
-    return <div>Home</div>
+export function HomePage() {
+	return (
+		<div className={styles.dashboardContainer}>
+			<nav className={styles.sidebar}>
+				<div className={styles.sidebarHeader}>
+					<h2>Menu</h2>
+				</div>
+				<ul className={styles.sidebarMenu}>
+					<li>
+						<Link className={styles.dashboardLink} to='/profile'>
+							Profile
+						</Link>
+					</li>
+					<li>
+						<Link className={styles.dashboardLink} to='/new-ride'>
+							New ride
+						</Link>
+					</li>
+					<li>
+						<Link className={styles.dashboardLink} to='/previous'>
+							Previous rides
+						</Link>
+					</li>
+					<li>
+						<Link
+							className={styles.dashboardLink}
+							to='/verification'
+						>
+							Verification
+						</Link>
+					</li>
+					<li>
+						<Link className={styles.dashboardLink} to='/new-ride'>
+							New rides
+						</Link>
+					</li>
+					<li>
+						<Link className={styles.dashboardLink} to='/my-rides'>
+							My rides
+						</Link>
+					</li>
+					<li>
+						<Link className={styles.dashboardLink} to='/all-rides'>
+							All rides
+						</Link>
+					</li>
+				</ul>
+			</nav>
+			<div className={styles.mainContent}>
+				<header className={styles.topbar}>
+					<h1>Dashboard</h1>
+					<div className={styles.userInfo}>
+						<p>Welcome, User</p>
+						<Link to='/logout'>Logout</Link>
+					</div>
+				</header>
+				<div>gas</div>
+			</div>
+		</div>
+	);
 }
